472697d248d8c47e0c802fd857f404416de99fcf
EllenCas172 <ellencas172@gmail.com>
2021-10-26 10:45:26 -0300

    organizando repositorio

--- src/ClinicaMedica/Interface.java
+++
@@ -1,5 +0,0 @@
-package ClinicaMedica;
-public interface Interface {
-    // Interface usada para os metodos que serão necessarios nas classes principais, que imprimimos e salvamos em binario
-    void mostrarDados();
-}
